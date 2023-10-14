6e61cbe1b0ac2a33cdc93e5be60752534e024257
gamma 吴 <116068704+wxsgamma@users.noreply.github.com>
2023-10-14 20:00:05 +0800

    Add files via upload

---
+++ exercises/macros/macros2.rs
@@ -0,0 +1,21 @@
+// macros2.rs
+//
+// Execute `rustlings hint macros2` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+
+mod my_module {
+    #[macro_export]
+    macro_rules! my_macro {
+            () => {
+        println!("Check out my macro!");
+    };
+    }
+}
+
+
+fn main() {
+    my_macro!();
+}
